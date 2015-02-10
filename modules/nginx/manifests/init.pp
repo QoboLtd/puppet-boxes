a7865f742aacb0d2c53fd7187d2188dcb44fb7de
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2015-02-10 13:48:43 +0200

    Adjust timezone and file upload in PHP config

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -158,4 +158,16 @@ class nginx {
 		mode => 755
 	}
 
+	file_line { 'date.timezone':
+		path => '/etc/php.ini',
+		line => 'date.timezone = "Asia/Nicosia"',
+		match => '^*date.timezone =.*',
+	}
+
+	file_line { 'upload_max_filesize':
+		path => '/etc/php.ini',
+		line => 'upload_max_filesize = 20M',
+		match => '^upload_max_filesize = 2\d*M',
+	}
+
 }
