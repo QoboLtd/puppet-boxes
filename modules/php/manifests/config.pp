f215e4e27eb2198d634983a262eef4c4c4df0dba
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2015-04-02 19:22:46 +0300

    Change post_max_size PHP setting to match upload_max_filesize
    * Added another variable handling to the manifest
    * Improved regex matching for both vars

--- modules/php/manifests/config.pp
+++ modules/php/manifests/config.pp
@@ -9,8 +9,13 @@ class php::config {
 	file_line { 'upload_max_filesize':
 		path => '/etc/php.ini',
 		line => 'upload_max_filesize = 20M',
-		match => '^upload_max_filesize = 2\d*M',
+		match => '^upload_max_filesize = \d+M',
 	}
 
+	file_line { 'post_max_size':
+		path => '/etc/php.ini',
+		line => 'post_max_size = 20M',
+		match => '^post_max_size = \d+M',
+	}
 
 }
